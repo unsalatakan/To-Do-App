1eab9266ecdd7652e811ac2cf4741f1817d97390
unsalatakan <45185182+unsalatakan@users.noreply.github.com>
2022-10-22 16:04:51 +0300

    Final

---
+++ app/src/main/java/com/example/kisileruygulamasi/room/KisilerDao.kt
@@ -0,0 +1,23 @@
+package com.example.kisileruygulamasi.room
+
+import androidx.room.*
+import com.example.kisileruygulamasi.data.entity.Kisiler
+
+@Dao
+interface KisilerDao { //Database Access Object
+    @Query("SELECT * FROM kisiler")
+    suspend fun kisileriYukle() : List<Kisiler>
+
+    @Insert
+    suspend fun kaydet(kisi:Kisiler)
+
+    @Update
+    suspend fun guncelle(kisi:Kisiler)
+
+    @Delete
+    suspend fun sil(kisi:Kisiler)
+
+    @Query("SELECT * FROM kisiler Where kisi_ad like '%' || :aramaKelimesi || '%'")
+    suspend fun ara(aramaKelimesi:String) : List<Kisiler>
+
+}
